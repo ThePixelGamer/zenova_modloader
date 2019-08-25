55f5a63cfc855efedf1b05d035bd775141b7b4f1
ThePixelCoder <thepixelmaster2354@gmail.com>
2019-08-25 17:21:02 -0500

    Start of a logger using the clipboard

--- ZenovaGUI/ZenovaGUI/main.cpp
+++ ZenovaGUI/ZenovaGUI/main.cpp
@@ -29,8 +29,10 @@ static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
 
 // Forward declarations of launcher functions
 void LaunchMinecraft(bool);
+void ShowLog(bool*);
 
 // Forward declarations of helper functions
+std::string GetClipboardText();
 bool CreateDeviceD3D(HWND);
 void CleanupDeviceD3D();
 void CreateRenderTarget();
@@ -60,7 +62,115 @@ void StateChangeCallbackFunc(PACKAGE_EXECUTION_STATE state) {
     }
 }
 
-#define ZENOVA_MSG 0xCDEF
+struct AppLog {
+    ImGuiTextBuffer     Buf;
+    ImGuiTextFilter     Filter;
+    ImVector<int>       LineOffsets;        // Index to lines offset. We maintain this with AddLog() calls, allowing us to have a random access on lines
+    bool                AutoScroll;     // Keep scrolling if already at the bottom
+
+    AppLog() {
+        AutoScroll = true;
+        Clear();
+    }
+
+    void Clear() {
+        Buf.clear();
+        LineOffsets.clear();
+        LineOffsets.push_back(0);
+    }
+
+    void AddLog(const char* fmt, ...) IM_FMTARGS(2) {
+        int old_size = Buf.size();
+        va_list args;
+        va_start(args, fmt);
+        Buf.appendfv(fmt, args);
+        va_end(args);
+        for (int new_size = Buf.size(); old_size < new_size; old_size++)
+            if (Buf[old_size] == '\n')
+                LineOffsets.push_back(old_size + 1);
+    }
+
+    void Draw(const char* title, bool* p_open = NULL) {
+        if (!ImGui::Begin(title, p_open)) {
+            ImGui::End();
+            return;
+        }
+
+        // Options menu
+        if (ImGui::BeginPopup("Options")) {
+            ImGui::Checkbox("Auto-scroll", &AutoScroll);
+            ImGui::EndPopup();
+        }
+
+        // Main window
+        if (ImGui::Button("Options"))
+            ImGui::OpenPopup("Options");
+        ImGui::SameLine();
+        bool clear = ImGui::Button("Clear");
+        ImGui::SameLine();
+        bool copy = ImGui::Button("Copy");
+        ImGui::SameLine();
+        Filter.Draw("Filter", -100.0f);
+
+        ImGui::Separator();
+        ImGui::BeginChild("scrolling", ImVec2(0,0), false, ImGuiWindowFlags_HorizontalScrollbar);
+
+        if (clear)
+            Clear();
+        if (copy)
+            ImGui::LogToClipboard();
+
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+        const char* buf = Buf.begin();
+        const char* buf_end = Buf.end();
+        if (Filter.IsActive()) {
+            // In this example we don't use the clipper when Filter is enabled.
+            // This is because we don't have a random access on the result on our filter.
+            // A real application processing logs with ten of thousands of entries may want to store the result of search/filter.
+            // especially if the filtering function is not trivial (e.g. reg-exp).
+            for (int line_no = 0; line_no < LineOffsets.Size; line_no++)
+            {
+                const char* line_start = buf + LineOffsets[line_no];
+                const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+                if (Filter.PassFilter(line_start, line_end))
+                    ImGui::TextUnformatted(line_start, line_end);
+            }
+        }
+        else {
+            // The simplest and easy way to display the entire buffer:
+            //   ImGui::TextUnformatted(buf_begin, buf_end);
+            // And it'll just work. TextUnformatted() has specialization for large blob of text and will fast-forward to skip non-visible lines.
+            // Here we instead demonstrate using the clipper to only process lines that are within the visible area.
+            // If you have tens of thousands of items and their processing cost is non-negligible, coarse clipping them on your side is recommended.
+            // Using ImGuiListClipper requires A) random access into your data, and B) items all being the  same height,
+            // both of which we can handle since we an array pointing to the beginning of each line of text.
+            // When using the filter (in the block of code above) we don't have random access into the data to display anymore, which is why we don't use the clipper.
+            // Storing or skimming through the search result would make it possible (and would be recommended if you want to search through tens of thousands of entries)
+            ImGuiListClipper clipper;
+            clipper.Begin(LineOffsets.Size);
+            while (clipper.Step())
+            {
+                for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
+                {
+                    const char* line_start = buf + LineOffsets[line_no];
+                    const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+                    ImGui::TextUnformatted(line_start, line_end);
+                }
+            }
+            clipper.End();
+        }
+        ImGui::PopStyleVar();
+
+        if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+            ImGui::SetScrollHereY(1.0f);
+
+        ImGui::EndChild();
+        ImGui::End();
+    }
+};
+
+#define BUF_SIZE 256
+CHAR szName[]=("Global\\ZenovaLogger");
 
 // Main code
 int wmain(int argumentsSize, wchar_t** arguments) {
@@ -116,6 +226,7 @@ int wmain(int argumentsSize, wchar_t** arguments) {
 
     // Our state
     bool show_demo_window = true;
+    bool show_app_log = true;
     bool launchMinecraft = false;
     bool show_mods_window = true;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
@@ -126,7 +237,6 @@ int wmain(int argumentsSize, wchar_t** arguments) {
 		app->setStateChangeCallback(StateChangeCallbackFunc);
 		StateChangeCallbackFunc(app->GetPackageExecutionState());
 	}
-	MSG messages;
 
 	//SetStateChangeCallback(Handler);
 
@@ -153,8 +263,8 @@ int wmain(int argumentsSize, wchar_t** arguments) {
         ImGui::NewFrame();
 
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
+		if (show_app_log)     ShowLog(&show_app_log);
 
         {
 			ImGui::Begin("App Launching Stuff");                          // Create a window called "Hello, world!" and append into it.
@@ -162,8 +272,7 @@ int wmain(int argumentsSize, wchar_t** arguments) {
 				launchMinecraft = true;
 
 			ImGui::Text("State: %s", currentState.c_str());
-			if(PeekMessageA(&messages, NULL, ZENOVA_MSG, ZENOVA_MSG, PM_REMOVE))
-				ImGui::Text("Log: (%x: %p)", messages.message, messages.lParam);
+
 			ImGui::End();
 		}
 
@@ -287,7 +396,54 @@ void LaunchMinecraft(bool forceRestart) {
 	return;//  S_OK;
 }
 
+void ShowLog(bool* p_open) {
+    static AppLog log;
+
+    // For the demo: add a debug button _BEFORE_ the normal log window contents
+    // We take advantage of a rarely used feature: multiple calls to Begin()/End() are appending to the _same_ window.
+    // Most of the contents of the window will be added by the log.Draw() call.
+    ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
+    ImGui::Begin("Zenova Logger", p_open);
+    {
+		SYSTEMTIME st;
+		GetLocalTime(&st);
+		log.AddLog("[%02d:%02d:%02d:%02d] [%s] '%s'\n",
+			st.wHour, st.wMinute, st.wSecond, st.wMilliseconds, "info", GetClipboardText().c_str());
+    }
+    ImGui::End();
+
+    // Actually call in the regular Log helper (which will Begin() into the same window as we just did)
+    log.Draw("Zenova Logger", p_open);
+}
+
 // Helper functions
+std::string GetClipboardText() {
+	// Try opening the clipboard
+	if(!OpenClipboard(nullptr)) {
+		std::cout << "OpenClipboard Failed" << std::endl;
+		return std::string();
+	}
+
+	// Get handle of clipboard object for ANSI text
+	HANDLE hData = GetClipboardData(CF_UNICODETEXT);
+	if (hData == nullptr) {
+		std::cout << "GetClipboardData Failed" << std::endl;
+		return std::string();
+	}
+
+	// Lock the handle to get the actual text pointer
+	char* pszText = static_cast<char*>(GlobalLock(hData));
+	if (pszText == nullptr) {
+		std::cout << "GlobalLock Failed" << std::endl;
+		return std::string();
+	}
+
+	std::string text(pszText); // Save text in a string class instance
+	GlobalUnlock(hData); // Release the lock
+	CloseClipboard(); // Release the clipboard
+
+	return text;
+}
 
 bool CreateDeviceD3D(HWND hWnd) {
     // Setup swap chain
